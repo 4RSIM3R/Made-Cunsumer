a9a4a885e9399dfa080cc3552f80f69e8933526a
Cak Suku <ilzammulkhaq85@gmail.com>
2019-08-10 21:42:09 +0700

    Content Provider !!!

--- app/src/main/java/com/studio/suku/made_consumer/LocalDb/MapHelper.java
+++ app/src/main/java/com/studio/suku/made_consumer/LocalDb/MapHelper.java
@@ -7,12 +7,21 @@ import java.util.ArrayList;
 import static com.studio.suku.made_consumer.LocalDb.DatabaseContract.Entry.COLUMN_NAME;
 
 public class MapHelper {
-    public static ArrayList<FavoriteItem> mapCursorToArrayList(Cursor favoriteCursor) {
+    public static ArrayList<FavoriteItem> mapCursorToArrayList(Cursor cursor) {
         ArrayList<FavoriteItem> notesList = new ArrayList<>();
-        while (favoriteCursor.moveToNext()) {
-            FavoriteItem favoriteItem = new FavoriteItem();
-            favoriteItem.setName(favoriteCursor.getString(favoriteCursor.getColumnIndexOrThrow(COLUMN_NAME)));
-        }
+        FavoriteItem favorite;
+        do {
+
+            favorite = new FavoriteItem();
+            favorite.setName(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.Entry.COLUMN_NAME)));
+            favorite.setImage(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.Entry.COLUMN_IMAGE)));
+            favorite.setOverview(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.Entry.COLUMN_OVERVIEW)));
+            favorite.setRate(cursor.getDouble(cursor.getColumnIndexOrThrow(DatabaseContract.Entry.COLUMN_RATE)));
+            favorite.setType(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.Entry.COLUMN_TYPE)));
+            notesList.add(favorite);
+            cursor.moveToNext();
+        }while (!cursor.isAfterLast());
+
         return notesList;
     }
 }
--- app/src/main/java/com/studio/suku/made_consumer/MainActivity.java
+++ app/src/main/java/com/studio/suku/made_consumer/MainActivity.java
@@ -3,7 +3,6 @@ package com.studio.suku.made_consumer;
 import android.content.Context;
 import android.database.ContentObserver;
 import android.database.Cursor;
-import android.database.DataSetObserver;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -11,9 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.studio.suku.made_consumer.Adapter.ConsumerAdapter;
+import com.studio.suku.made_consumer.LocalDb.DatabaseContract;
 import com.studio.suku.made_consumer.LocalDb.Favorite;
 import com.studio.suku.made_consumer.LocalDb.FavoriteItem;
 import com.studio.suku.made_consumer.LocalDb.LoadFavoriteCallback;
@@ -22,13 +23,14 @@ import com.studio.suku.made_consumer.LocalDb.MapHelper;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
-import static com.studio.suku.made_consumer.LocalDb.DatabaseContract.Entry.CONTENT_URI;
+import static com.studio.suku.made_consumer.LocalDb.DatabaseContract.Entry.*;
 
 public class MainActivity extends AppCompatActivity implements LoadFavoriteCallback {
 
     private ConsumerAdapter consumerAdapter;
     private MainActivity.DataObserver myObserver;
     RecyclerView list;
+    ArrayList<FavoriteItem> favoriteArrayList;
 
 
 
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements LoadFavoriteCallb
         consumerAdapter = new ConsumerAdapter(this);
         list.setLayoutManager(new LinearLayoutManager(this));
         list.setHasFixedSize(true);
-        list.setAdapter(consumerAdapter);
+        //list.setAdapter(consumerAdapter);
         HandlerThread handlerThread = new HandlerThread("DataObserver");
         handlerThread.start();
         Handler handler = new Handler(handlerThread.getLooper());
@@ -49,16 +51,19 @@ public class MainActivity extends AppCompatActivity implements LoadFavoriteCallb
         getContentResolver().registerContentObserver(CONTENT_URI, true, myObserver);
         new getData(this, this).execute();
 
+
+
+
     }
 
     @Override
     public void postExecute(Cursor favorites) {
-        ArrayList<FavoriteItem> list = MapHelper.mapCursorToArrayList(favorites);
-        if (list.size() > 0) {
-            consumerAdapter.setListFavorites(list);
-        } else {
-            Toast.makeText(this, "Tidak Ada data saat ini", Toast.LENGTH_SHORT).show();
-            consumerAdapter.setListFavorites(new ArrayList<FavoriteItem>());
+        //ArrayList<FavoriteItem> list = MapHelper.mapCursorToArrayList(favorites);
+        if (favorites != null){
+            Log.d("Berhasil", "Berhasil");
+        }
+        else {
+            Log.d("Berhasil", "Gagal");
         }
     }
 
